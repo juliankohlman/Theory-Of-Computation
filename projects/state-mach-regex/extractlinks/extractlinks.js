d08a8b7b006b706fb39da91dee73c03db5516fe7
Julian Kohlman <juliankohlman@gmail.com>
2018-04-16 17:48:10 -0400

    extractlinks working

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,18 +1,40 @@
-const fs = require('fs');
+const fs = require("fs");
+const readline = require('readline');
+const stream = require('stream');
+
 
 const args = process.argv.slice(2);
 
-if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+if (args.length !== 1) {
+  console.error("usage: extractlinks inputfile");
+  process.exit(1);
 }
 
 const filename = args[0];
 
+const inStream = fs.createReadStream(filename);
+const outStream = new stream;
+const rl = readline.createInterface(inStream, outStream);
+const url = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+
+rl.on('line', (line) => {
+  // console.log(line.match(url)[0]);
+  if (line.match(url)) {
+    console.log(line.match(url)[0]);
+  }
+  // console.log(line);
+});
+
+rl.on('close', function() {
+  console.log('done');
+});
 // !!!! IMPLEMENT ME
 
 // Read file
-
+// fs.readFile(filename, 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 // Set up regex
 
 // Find matches

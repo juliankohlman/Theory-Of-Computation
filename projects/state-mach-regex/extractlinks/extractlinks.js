3e1a874e0547f67fe2d804e4ee285b35be520324
Julian Kohlman <juliankohlman@gmail.com>
2018-04-16 17:58:09 -0400

    Add line count to link output

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -17,24 +17,21 @@ const outStream = new stream;
 const rl = readline.createInterface(inStream, outStream);
 const url = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
+let lineNumber = 0;
 rl.on('line', (line) => {
-  // console.log(line.match(url)[0]);
   if (line.match(url)) {
-    console.log(line.match(url)[0]);
+    lineNumber++;
+    console.log(`${lineNumber}: ${line.match(url)[0]}`);
   }
-  // console.log(line);
 });
 
 rl.on('close', function() {
-  console.log('done');
+  console.log(`Done there are ${lineNumber} links on the page.`);
 });
 // !!!! IMPLEMENT ME
 
 // Read file
-// fs.readFile(filename, 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
+
 // Set up regex
 
 // Find matches
